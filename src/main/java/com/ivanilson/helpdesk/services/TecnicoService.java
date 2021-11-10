7ed532efada0c4ddd2a8e70add76539dacc3bd03
= <=>
2021-11-10 13:52:42 -0300

    endpoint create para Tecnico

--- src/main/java/com/ivanilson/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ivanilson/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ivanilson.helpdesk.domain.Tecnico;
+import com.ivanilson.helpdesk.domain.dtos.TecnicoDTO;
 import com.ivanilson.helpdesk.repositories.TecnicoRepository;
 import com.ivanilson.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 }
