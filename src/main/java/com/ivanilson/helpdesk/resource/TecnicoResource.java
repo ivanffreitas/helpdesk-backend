6ec48d6f95861b84fabeab065e4856b9950fc072
= <=>
2021-11-11 09:40:08 -0300

    Metodo PUT para Tecnico implementado

--- src/main/java/com/ivanilson/helpdesk/resource/TecnicoResource.java
+++ src/main/java/com/ivanilson/helpdesk/resource/TecnicoResource.java
@@ -11,8 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -45,6 +47,12 @@ public class TecnicoResource {
 		Tecnico newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO ojDto) {
+		Tecnico obj = service.update(id, ojDto);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
 
