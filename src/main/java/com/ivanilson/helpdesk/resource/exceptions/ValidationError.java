2862dd3210427ab2aad7d09e1f97ee7d5726fb93
= <=>
2021-11-11 08:16:54 -0300

    Validacao nos campos de criacao de novo usuario

---
+++ src/main/java/com/ivanilson/helpdesk/resource/exceptions/ValidationError.java
@@ -0,0 +1,29 @@
+package com.ivanilson.helpdesk.resource.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ivanilson.helpdesk.services.exceptions.StandarError;
+
+public class ValidationError extends StandarError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError() {
+		super();
+	}
+
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fielName, String message) {
+		this.errors.add(new FieldMessage(fielName, message));
+	}
+
+}
