ceae8d5beac47ecd9c1ae7f560c2efa52be84fe8
ivanffreitas <ivanff.ti@gmail.com>
2021-11-16 07:29:19 -0300

    create para chamado

--- src/main/java/com/ivanilson/helpdesk/resource/ChamadoResource.java
+++ src/main/java/com/ivanilson/helpdesk/resource/ChamadoResource.java
@@ -1,14 +1,21 @@
 package com.ivanilson.helpdesk.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ivanilson.helpdesk.domain.Chamado;
 import com.ivanilson.helpdesk.domain.dtos.ChamadoDTO;
@@ -32,6 +39,14 @@ public class ChamadoResource {
 		List<Chamado> list = service.findAll();
 		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+		Chamado obj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId())	.toUri();
+		return ResponseEntity.created(uri).build();		
 	}
 	
 }
